0c0373d03d27907e2791369f787a4b79ee359a7d
Ronald <ronald.lou88@gmail.com>
2020-05-26 11:26:33 -0500

    add: top destination carousel :octocat:

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel_ui/widgets/destination_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -70,6 +71,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   )
                   .toList(),
             ),
+            SizedBox(height: 20.0),
+            DestinationCarousel(),
           ],
         ),
       ),
